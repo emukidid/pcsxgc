dfbeb8641e3c18d77b0d15c9881553e58d185e2c
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2023-02-26 14:53:18 -0500

    Autoboot support for WiiFlow via arguments. (Wiimpathy)

--- Gamecube/GamecubeMain.cpp
+++ Gamecube/GamecubeMain.cpp
@@ -330,10 +330,26 @@ PluginTable plugins[] =
 	  PLUGIN_SLOT_5 };
 }
 
+bool Autoboot;
+char AutobootROM[1024];
+char AutobootPath[1024];
+
 int main(int argc, char *argv[]) 
 {
 	/* INITIALIZE */
 #ifdef HW_RVL
+	if(argc > 2 && argv[1] != NULL && argv[2] != NULL)
+	{
+		Autoboot = true;
+		strncpy(AutobootPath, argv[1], sizeof(AutobootPath));
+		strncpy(AutobootROM, argv[2], sizeof(AutobootROM));
+	}
+	else
+	{
+		Autoboot = false;
+		memset(AutobootPath, 0, sizeof(AutobootPath));
+		memset(AutobootROM, 0, sizeof(AutobootROM));
+	}
 	VM_Init(1024*1024, 256*1024); // whatever for now, we're not really using this for anything other than mmap on Wii.
 	DI_UseCache(false);
 	if (!__di_check_ahbprot()) {
@@ -377,9 +393,15 @@ int main(int argc, char *argv[])
 	  init_network_thread();
   }
 #endif
+
+	if(Autoboot)
+	{
+		menu->Autoboot();
+		Autoboot = false;
+	}
 	
 	while (menu->isRunning()) {}
-	
+
 	// Shut down AESND
 	AESND_Reset();
 
