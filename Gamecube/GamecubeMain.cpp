616dec543b6b23a0fd5e633030553965d25e64e3
emu_kidid <emukidid@gmail.com>
2023-04-12 17:28:06 +0930

    - Init Config.cycle_multiplier to a default value.

--- Gamecube/GamecubeMain.cpp
+++ Gamecube/GamecubeMain.cpp
@@ -206,6 +206,7 @@ void loadSettings(int argc, char *argv[])
 	spu_config.iXAPitch = 0;
 	spu_config.iTempo = 0;
 	Config.PsxAuto = 1; //Autodetect
+	Config.cycle_multiplier = CYCLE_MULT_DEFAULT;
 	LoadCdBios = BOOTTHRUBIOS_NO;
 
 	//config stuff
